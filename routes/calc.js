d830c35d21efb42529a5fb02b47d89adaa27f9ed
Eddy Soma <esoma92.dev@gmail.com>
2019-02-07 00:18:14 -0500

    add type symbol images

--- routes/calc.js
+++ routes/calc.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var renderData = {
-  title: "Pokémon Damage Calculator"
+  title: "Pokémon Calculator"
 };
 
 /* GET home page. */
